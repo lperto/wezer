23ff9d0b7905cee3260c992e7049a0066e3b35b1
Lasha Pertakhia <lperto@gmail.com>
2024-01-04 21:38:15 +0400

    fixed get_weather

--- get_weather.py
+++ get_weather.py
@@ -1,6 +1,15 @@
+import os
 import requests
 import csv
 from datetime import datetime
+
+
+def get_api_key():
+    api_key = os.environ.get("OPENWEATHERMAP_API_KEY")
+    if not api_key:
+        raise ValueError("OpenWeatherMap API key not found in environment variables.")
+    return api_key
+
 
 def get_weather(api_key, lat, lon):
     url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={api_key}"
@@ -11,22 +20,24 @@ def get_weather(api_key, lat, lon):
     data = response.json()
     print(data)
 
-    temperature = data['main']['temp']
-    weather_description = data['weather'][0]['description']
+    temperature = data["main"]["temp"]
+    weather_description = data["weather"][0]["description"]
 
     return temperature, weather_description
+
 
 def update_csv(file_path, lat, lon, api_key):
     temperature, weather_description = get_weather(api_key, lat, lon)
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    with open(file_path, 'a', newline='') as csvfile:
+    with open(file_path, "a", newline="") as csvfile:
         csv_writer = csv.writer(csvfile)
         # csv_writer.writerow([timestamp, lat, lon, temperature, weather_description])
         csv_writer.writerow([timestamp, temperature, weather_description])
 
+
 if __name__ == "__main__":
-    api_key = "YOUR_OPENWEATHERMAP_API_KEY"
+    api_key = get_api_key()
     lat, lon = [41.6941, 44.8337]
     file_path = "weather_data.csv"
 
